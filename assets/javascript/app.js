e3802ddc2b92912c01b6f98bc39b887cbe901d7f
unknown <mstange22@gmail.com>
2017-08-31 21:56:59 -0700

    random order, harder choices

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,6 +17,258 @@ var correctAnswer = false;
 var isUnanswered = false;
 var correctCapital = "";
 
+// The game array that will hold all of the states and answers
+var game = [{state: "Alabama", answers: [ {answer: "Montgomery", isCorrect: true},
+										{answer: "Tuscaloosa", iscorrect: false},
+										{answer: "Birmingham", isCorrect: false},
+										{answer: "Jackson", isCorrect: false} ]},
+
+			{state: "Alaska", answers: [ {answer: "Juneau", isCorrect: true},
+										{answer: "Anchorage", iscorrect: false},
+										{answer: "Fairbanks", isCorrect: false},
+										{answer: "Olympia", isCorrect: false} ]},
+
+			{state: "Arizona", answers: [ {answer: "Phoenix", isCorrect: true},
+										{answer: "Tucson", iscorrect: false},
+										{answer: "Tempe", isCorrect: false},
+										{answer: "Albequerque", isCorrect: false} ]},
+
+			{state: "Arkansas", answers: [ {answer: "Little Rock", isCorrect: true},
+										{answer: "Fort Smith", iscorrect: false},
+										{answer: "Jefferson City", isCorrect: false},
+										{answer: "Jackson", isCorrect: false} ]},
+
+			{state: "California", answers: [ {answer: "Sacramento", isCorrect: true},
+										{answer: "Los Angeles", iscorrect: false},
+										{answer: "San Francisco", isCorrect: false},
+										{answer: "Carson", isCorrect: false} ]},
+
+			{state: "Colorado", answers: [ {answer: "Denver", isCorrect: true},
+										{answer: "Boulder", iscorrect: false},
+										{answer: "Fort Collins", isCorrect: false},
+										{answer: "Lincoln", isCorrect: false} ]},
+
+			{state: "Connecticut", answers: [ {answer: "Hartford", isCorrect: true},
+										{answer: "New Haven", iscorrect: false},
+										{answer: "Bridgeport", isCorrect: false},
+										{answer: "Concord", isCorrect: false} ]},
+
+			{state: "Delaware", answers: [ {answer: "Dover", isCorrect: true},
+										{answer: "Willmington", iscorrect: false},
+										{answer: "Newark", isCorrect: false},
+										{answer: "Annapolis", isCorrect: false} ]},
+
+			{state: "Florida", answers: [ {answer: "Tallahassee", isCorrect: true},
+										{answer: "Miami", iscorrect: false},
+										{answer: "Orlando", isCorrect: false},
+										{answer: "Jacksonville", isCorrect: false} ]},
+
+			{state: "Georgia", answers: [ {answer: "Atlanta", isCorrect: true},
+										{answer: "Columbus", iscorrect: false},
+										{answer: "Savannah", isCorrect: false},
+										{answer: "Auburn", isCorrect: false} ]},
+
+			{state: "Hawaii", answers: [ {answer: "Honolulu", isCorrect: true},
+										{answer: "Hilo", iscorrect: false},
+										{answer: "Kailua-Kona", isCorrect: false},
+										{answer: "Pearl City", isCorrect: false} ]},
+
+			{state: "Idaho", answers: [ {answer: "Boise", isCorrect: true},
+										{answer: "Coeur d'Alene", iscorrect: false},
+										{answer: "Helena", isCorrect: false},
+										{answer: "Des Moines", isCorrect: false} ]},
+
+			{state: "Illinois", answers: [ {answer: "Springfield", isCorrect: true},
+										{answer: "Chicago", iscorrect: false},
+										{answer: "Madison", isCorrect: false},
+										{answer: "Peoria", isCorrect: false} ]},
+
+			{state: "Indiana", answers: [ {answer: "Indianapolis", isCorrect: true},
+										{answer: "Bloomington", iscorrect: false},
+										{answer: "Fort Wayne", isCorrect: false},
+										{answer: "Springfield", isCorrect: false} ]},
+
+			{state: "Iowa", answers: [ {answer: "Des Moines", isCorrect: true},
+										{answer: "Iowa City", iscorrect: false},
+										{answer: "Ames", isCorrect: false},
+										{answer: "Pierre", isCorrect: false} ]},
+
+			{state: "Kansas", answers: [ {answer: "Topeka", isCorrect: true},
+										{answer: "Wichita", iscorrect: false},
+										{answer: "Kansas City", isCorrect: false},
+										{answer: "St. Louis", isCorrect: false} ]},
+
+			{state: "Kentucky", answers: [ {answer: "Frankfort", isCorrect: true},
+										{answer: "Louisville", iscorrect: false},
+										{answer: "Lexington", isCorrect: false},
+										{answer: "Nashville", isCorrect: false} ]},
+
+			{state: "Louisiana", answers: [ {answer: "Baton Rouge", isCorrect: true},
+										{answer: "New Orleans", iscorrect: false},
+										{answer: "Shreveport", isCorrect: false},
+										{answer: "Jackson", isCorrect: false} ]},
+
+			{state: "Maine", answers: [ {answer: "Augusta", isCorrect: true},
+										{answer: "Portland", iscorrect: false},
+										{answer: "Albany", isCorrect: false},
+										{answer: "Concord", isCorrect: false} ]},
+
+			{state: "Maryland", answers: [ {answer: "Annapolis", isCorrect: true},
+										{answer: "Baltimore", iscorrect: false},
+										{answer: "Bethesda", isCorrect: false},
+										{answer: "Charleston", isCorrect: false} ]},
+
+			{state: "Massachusetts", answers: [ {answer: "Boston", isCorrect: true},
+										{answer: "Springfield", iscorrect: false},
+										{answer: "Amherst", isCorrect: false},
+										{answer: "Hartford", isCorrect: false} ]},
+
+			{state: "Michigan", answers: [ {answer: "Lansing", isCorrect: true},
+										{answer: "Detroit", iscorrect: false},
+										{answer: "Saint Paul", isCorrect: false},
+										{answer: "Springfield", isCorrect: false} ]},
+
+			{state: "Minnesota", answers: [ {answer: "Saint Paul", isCorrect: true},
+										{answer: "Minneapolis", iscorrect: false},
+										{answer: "Duluth", isCorrect: false},
+										{answer: "Madison", isCorrect: false} ]},
+
+			{state: "Mississippi", answers: [ {answer: "Jackson", isCorrect: true},
+										{answer: "Madison", iscorrect: false},
+										{answer: "Columbus", isCorrect: false},
+										{answer: "Jefferson City", isCorrect: false} ]},
+
+			{state: "Missouri", answers: [ {answer: "Jefferson City", isCorrect: true},
+										{answer: "St. Louis", iscorrect: false},
+										{answer: "Jackson", isCorrect: false},
+										{answer: "Springfield", isCorrect: false} ]},
+
+			{state: "Montana", answers: [ {answer: "Helena", isCorrect: true},
+										{answer: "Missoula", iscorrect: false},
+										{answer: "Bismarck", isCorrect: false},
+										{answer: "Pierre", isCorrect: false} ]},
+
+			{state: "Nebraska", answers: [ {answer: "Lincoln", isCorrect: true},
+										{answer: "Omaha", iscorrect: false},
+										{answer: "Ames", isCorrect: false},
+										{answer: "Topeka", isCorrect: false} ]},
+
+			{state: "Nevada", answers: [ {answer: "Carson City", isCorrect: true},
+										{answer: "Las Vegas", iscorrect: false},
+										{answer: "Reno", isCorrect: false},
+										{answer: "Carson", isCorrect: false} ]},
+
+			{state: "New Hampshire", answers: [ {answer: "Concord", isCorrect: true},
+										{answer: "Manchester", iscorrect: false},
+										{answer: "Dover", isCorrect: false},
+										{answer: "Montpelier", isCorrect: false} ]},
+
+			{state: "New Jersey", answers: [ {answer: "Trenton", isCorrect: true},
+										{answer: "Jersey City", iscorrect: false},
+										{answer: "Newark", isCorrect: false},
+										{answer: "Annapolis", isCorrect: false} ]},
+
+			{state: "New Mexico", answers: [ {answer: "Santa Fe", isCorrect: true},
+										{answer: "Albequerque", iscorrect: false},
+										{answer: "Las Cruces", isCorrect: false},
+										{answer: "Bismarck", isCorrect: false} ]},
+
+			{state: "New York", answers: [ {answer: "Albany", isCorrect: true},
+										{answer: "New York City", iscorrect: false},
+										{answer: "Utica", isCorrect: false},
+										{answer: "Concord", isCorrect: false} ]},
+
+			{state: "North Carolina", answers: [ {answer: "Raleigh", isCorrect: true},
+										{answer: "Willmington", iscorrect: false},
+										{answer: "Charlotte", isCorrect: false},
+										{answer: "Columbia", isCorrect: false} ]},
+
+			{state: "North Dakota", answers: [ {answer: "Bismarck", isCorrect: true},
+										{answer: "Fargo", iscorrect: false},
+										{answer: "Grand Forks", isCorrect: false},
+										{answer: "Pierre", isCorrect: false} ]},
+
+			{state: "Ohio", answers: [ {answer: "Columbus", isCorrect: true},
+										{answer: "Cleveland", iscorrect: false},
+										{answer: "Cincinnati", isCorrect: false},
+										{answer: "Toledo", isCorrect: false} ]},
+
+			{state: "Oklahoma", answers: [ {answer: "Oklahoma City", isCorrect: true},
+										{answer: "Tulsa", iscorrect: false},
+										{answer: "Norman", isCorrect: false},
+										{answer: "Wichita", isCorrect: false} ]},
+
+			{state: "Oregon", answers: [ {answer: "Salem", isCorrect: true},
+										{answer: "Eugene", iscorrect: false},
+										{answer: "Portland", isCorrect: false},
+										{answer: "Olympia", isCorrect: false} ]},
+
+			{state: "Pennsylvania", answers: [ {answer: "Harrisburg", isCorrect: true},
+										{answer: "Pittsburgh", iscorrect: false},
+										{answer: "Philadelphia", isCorrect: false},
+										{answer: "Dover", isCorrect: false} ]},
+
+			{state: "Rhode Island", answers: [ {answer: "Providence", isCorrect: true},
+										{answer: "Dover", iscorrect: false},
+										{answer: "Hartford", isCorrect: false},
+										{answer: "Newport", isCorrect: false} ]},
+
+			{state: "South Carolina", answers: [ {answer: "Columbia", isCorrect: true},
+										{answer: "Charleston", iscorrect: false},
+										{answer: "Greenville", isCorrect: false},
+										{answer: "Raleigh", isCorrect: false} ]},
+
+			{state: "South Dakota", answers: [ {answer: "Pierre", isCorrect: true},
+										{answer: "Sioux Falls", iscorrect: false},
+										{answer: "Rapid City", isCorrect: false},
+										{answer: "Bismarck", isCorrect: false} ]},
+
+			{state: "Tennessee", answers: [ {answer: "Nashville", isCorrect: true},
+										{answer: "Memphis", iscorrect: false},
+										{answer: "Knoxville", isCorrect: false},
+										{answer: "Chattanooga", isCorrect: false} ]},
+
+			{state: "Texas", answers: [ {answer: "Austin", isCorrect: true},
+										{answer: "Dallas", iscorrect: false},
+										{answer: "Fort Worth", isCorrect: false},
+										{answer: "Houston", isCorrect: false} ]},
+
+			{state: "Utah", answers: [ {answer: "Salt Lake City", isCorrect: true},
+										{answer: "Saint George", iscorrect: false},
+										{answer: "Provo", isCorrect: false},
+										{answer: "Ogden", isCorrect: false} ]},
+
+			{state: "Vermont", answers: [ {answer: "Montpelier", isCorrect: true},
+										{answer: "Concord", iscorrect: false},
+										{answer: "Springfield", isCorrect: false},
+										{answer: "Burlington", isCorrect: false} ]},
+
+			{state: "Virginia", answers: [ {answer: "Richmond", isCorrect: true},
+										{answer: "Charlottesville", iscorrect: false},
+										{answer: "Norfolk", isCorrect: false},
+										{answer: "Alexandria", isCorrect: false} ]},
+
+			{state: "Washington", answers: [ {answer: "Olympia", isCorrect: true},
+										{answer: "Seattle", iscorrect: false},
+										{answer: "Tacoma", isCorrect: false},
+										{answer: "Richland", isCorrect: false} ]},
+
+			{state: "West Virginia", answers: [ {answer: "Charleston", isCorrect: true},
+										{answer: "Richmond", iscorrect: false},
+										{answer: "Charlotte", isCorrect: false},
+										{answer: "Columbia", isCorrect: false} ]},
+
+			{state: "Wisconsin", answers: [ {answer: "Madison", isCorrect: true},
+										{answer: "Green Bay", iscorrect: false},
+										{answer: "Milwaukee", isCorrect: false},
+										{answer: "Springfield", isCorrect: false} ]},
+
+			{state: "Wyoming", answers: [ {answer: "Cheyenne", isCorrect: true},
+										{answer: "Jackson", iscorrect: false},
+										{answer: "Helena", isCorrect: false},
+										{answer: "Pierre", isCorrect: false} ]}
+];
+
 var states = [
 	"Alabama", "Alaska", "Arizona", "Arkansas", "California",
     "Colorado", "Connecticut", "Delaware", "Florida", "Georgia",
@@ -38,14 +290,21 @@ var capitals = [
     "Topeka", "Frankfort", "Baton Rouge", "Augusta", "Annapolis",
     "Boston", "Lansing", "Saint Paul", "Jackson", "Jefferson City",
     "Helena", "Lincoln", "Carson City", "Concord", "Trenton",
-    "Santa Fe", "Albany", "Raleigh", "Bismark", "Columbus",
+    "Santa Fe", "Albany", "Raleigh", "Bismarck", "Columbus",
     "Oklahoma City", "Salem", "Harrisburg", "Providence", "Columbia",
     "Pierre", "Nashville", "Austin", "Salt Lake City", "Montpelier",
     "Richmond", "Olympia", "Charleston", "Madison", "Cheyenne"
 ];
 
 // The game array that will hold all of the states and answers
-var game = [];
+var game2 = [];
+
+// array of indexes for random order of states displayed
+var randomStateNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+						 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
+						 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+						 41, 42, 43, 44, 45, 46, 47, 48, 49];
+var randomState = 0;
 
 // Objects for each answer for a given state
 var answer1 = {};
@@ -61,7 +320,7 @@ $(document).ready(function() {
 		$("#start-button").css("display", "none");
 		$(".jumbotron").css("background-image", "none");
 
-		getQuestions();
+		// getQuestions();
 		play();
 	});
 
@@ -92,6 +351,12 @@ $(document).ready(function() {
 			numIncorrect = 0;
 			numUnanswered = 0;
 			questionCounter = 0;
+	
+			randomStateNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+								 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
+								 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+								 41, 42, 43, 44, 45, 46, 47, 48, 49];
+			randomState = 0;
 
 			reset();		
 	});
@@ -187,23 +452,25 @@ function play() {
  */
 function askQuestion() {
 
+	$("#pause-button").css("display", "none");
+
+	randomState = randomStateNumber.splice(Math.floor(Math.random() *
+													randomStateNumber.length), 1);
 	// capture the correct capital
 	correctCapital = game[questionCounter].answers[0];
 
 	var randomNumbers = [0, 1, 2, 3];
 
-	Math.floor(Math.random() * randomNumbers.length)
-
 	// Display the state
-	$("#state").html("<h2>" + game[questionCounter].state + "</h2>");
+	$("#state").html("<h2>" + game[randomState].state + "</h2>");
 
-	answer1 = game[questionCounter].answers[randomNumbers.splice(Math.floor(Math.random() *
+	answer1 = game[randomState].answers[randomNumbers.splice(Math.floor(Math.random() *
 													randomNumbers.length), 1)];
-	answer2 = game[questionCounter].answers[randomNumbers.splice(Math.floor(Math.random() *
+	answer2 = game[randomState].answers[randomNumbers.splice(Math.floor(Math.random() *
 													randomNumbers.length), 1)];
-	answer3 = game[questionCounter].answers[randomNumbers.splice(Math.floor(Math.random() *
+	answer3 = game[randomState].answers[randomNumbers.splice(Math.floor(Math.random() *
 													randomNumbers.length), 1)];
-	answer4 = game[questionCounter].answers[randomNumbers.splice(Math.floor(Math.random() *
+	answer4 = game[randomState].answers[randomNumbers.splice(Math.floor(Math.random() *
 													randomNumbers.length), 1)];
 
 	// Display the answers in random order
@@ -261,10 +528,10 @@ function displayGameResults() {
 
 	if(isUnanswered) {
 
-		$("#message").html("Unanswered");
+		$("#timer").html("<h2>Unanswered</h2>");
 		$("#message").append("<h3>The capital is of <b>" +
-					game[questionCounter - 1].state + "</b> is <b>" +
-					game[questionCounter - 1].answers[0].answer + "</b></h3>");
+					game[randomState].state + "</b> is <b>" +
+					game[randomState].answers[0].answer + "</b></h3>");
 		numUnanswered++;
 		isUnanswered = false;
 	}
@@ -273,17 +540,17 @@ function displayGameResults() {
 
 		if(correctAnswer) {
 
-			$("#message").html("Correct!");
+			$("#timer").html("<h2>Correct!</h2>");
 			numCorrect++;
 			correctAnswer = false;
 		}
 
 		else {
 
-		$("#message").html("Incorrect");
+		$("#timer").html("<h2>Incorrect</h2>");
 		$("#message").append("<h3>The capital is of <b>" +
-					game[questionCounter - 1].state + "</b> is <b>" +
-					game[questionCounter - 1].answers[0].answer + "</b></h3>");
+					game[randomState].state + "</b> is <b>" +
+					game[randomState].answers[0].answer + "</b></h3>");
 		numIncorrect++;
 		correctAnswer = false;
 		}
@@ -292,6 +559,8 @@ function displayGameResults() {
 	$("#results").html("<h3>Correct Answers: " + numCorrect + "</h3>");
 	$("#results").append("<h3>Incorrect Answers: " + numIncorrect + "</h3>");
 	$("#results").append("<h3>Unanswered: " + numUnanswered + "</h3>");
+
+	$("#pause-button").css("display", "block");
 }
 
 function decrement() {
